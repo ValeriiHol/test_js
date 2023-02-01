1b3bb5f1c84e1eff32102de97372da1f6e93f820
ValeriiHol <rielty@gmail.com>
2023-02-01 13:35:26 +0200

    test for js

--- js/test.js
+++ js/test.js
@@ -1,10 +1,24 @@
-// Декларативний підхід
-const numbers = [1, 2, 3, 4, 5];
-const filteredNumbers = numbers.filter(value => value > 3);
-console.log(filteredNumbers); // [4, 5]
-  
+const LOW_SCORE = 50;
+const HIGH_SCORE = 80;
+const students = [
+  { name: "Манго", score: 83 },
+  { name: "Полі", score: 59 },
+  { name: "Аякс", score: 37 },
+  { name: "Ківі", score: 94 },
+  { name: "Х'юстон", score: 64 },
+];
 
-   
+const best = students.filter(student => student.score >= HIGH_SCORE);
+console.log(best); // Масив об'єктів з іменами Манго і Ківі
+
+const worst = students.filter(student => student.score < LOW_SCORE);
+console.log(worst); // Масив з одним об'єктом Аякс
+
+// В колбек-функції зручно деструктуризувати властивості об'єкта
+const average = students.filter(
+  ({ score }) => score >= LOW_SCORE && score < HIGH_SCORE
+);
+console.log(average); // Масив об'єктів з іменами Полі і Х'юстон
   
 
 // console.log(bookShelf.getBooks()); 
