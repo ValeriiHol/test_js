a834eb069b55af1decc4bee96379a005e5c26212
ValeriiHol <rielty@gmail.com>
2023-02-01 13:02:17 +0200

    test for js

---
+++ js/test.js
@@ -0,0 +1,16 @@
+// Декларативний підхід
+const numbers = [1, 2, 3, 4, 5];
+const filteredNumbers = numbers.filter(value => value > 3);
+console.log(filteredNumbers); // [4, 5]
+  
+
+   
+  
+
+// console.log(bookShelf.getBooks()); 
+// console.log(bookShelf.addBook("Haze")); 
+// console.log(bookShelf.removeBook("Red sunset")); 
+// console.log(bookShelf.updateBook("Sands of dune", "Dune")); 
+// console.log(makeTask({ text: "Buy bread" })); 
+console.log(); 
+
