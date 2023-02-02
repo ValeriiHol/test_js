6796444f88db7c8e5e4b6810fc94ff4679d8bb5c
ValeriiHol <rielty@gmail.com>
2023-02-02 21:04:54 +0200

    последнее 41 задание 3 модуля

--- js/test.js
+++ js/test.js
@@ -1,25 +1,55 @@
-const LOW_SCORE = 50;
-const HIGH_SCORE = 80;
-const students = [
-  { name: "Манго", score: 83 },
-  { name: "Полі", score: 59 },
-  { name: "Аякс", score: 37 },
-  { name: "Ківі", score: 94 },
-  { name: "Х'юстон", score: 64 },
-];
-
-const best = students.filter(student => student.score >= HIGH_SCORE);
-console.log(best); // Масив об'єктів з іменами Манго і Ківі
-
-const worst = students.filter(student => student.score < LOW_SCORE);
-console.log(worst); // Масив з одним об'єктом Аякс
-
-// В колбек-функції зручно деструктуризувати властивості об'єкта
-const average = students.filter(
-  ({ score }) => score >= LOW_SCORE && score < HIGH_SCORE
-);
-console.log(average); // Масив об'єктів з іменами Полі і Х'юстон
+const atTheOldToad = {
+    potions: [
+      { name: "Speed potion", price: 460 },
+      { name: "Dragon breath", price: 780 },
+      { name: "Stone skin", price: 520 },
+    ],
+    // Change code below this line
+    getPotions() {
+      return this.potions;
+    },  
+    addPotion(newPotion) {
+        for (const potion of this.potions) {
+            if (potion.name === newPotion.name) {
+                return `Error! Potion ${newPotion} is already in your inventory!`;
+            }
+        }
+      this.potions.push(newPotion);
+    },
+    removePotion(potionName) {        
+        for (let i = 0; i < this.potions.length; i += 1 ) {
+            console.log(this.potions[i]);  
+            if (potionName === this.potions[i].name) {
+                console.log(this.potions[i].name);  
+                this.potions.splice(i, 1);
+            }
+        }
+        return `Potion ${potionName} is not in inventory!`;
+    },
+    updatePotionName(oldName, newName) {
+        for (let i = 0; i < this.potions.length; i += 1 ) {
+            console.log(this.potions[i]);  
+            if (oldName === this.potions[i].name) {
+                console.log(this.potions[i].name);  
+                this.potions[i].name = newName;
+            }
+        }    
+    },
+    // Change code above this line
+  };
   
+// atTheOldToad.addPotion({ name: "Invisibility", price: 620 }); 
+//     console.log(atTheOldToad.getPotions()); 
+//     atTheOldToad.addPotion({ name: "Power potion", price: 270 });
+//     console.log(atTheOldToad.getPotions()); 
+// console.log(atTheOldToad.addPotion({ name: "SSSSbreath", price: 700 })); 
+console.log(atTheOldToad.addPotion({ name: "Dragon breath", price: 700 })); 
+
+// atTheOldToad.updatePotionName("Dragon breath", "Polymorth");
+// console.log(atTheOldToad.getPotions()); 
+    // atTheOldToad.removePotion("Dragon breath");
+    console.log(atTheOldToad.getPotions()); 
+
 
 // console.log(bookShelf.getBooks()); 
 // console.log(bookShelf.addBook("Haze")); 
